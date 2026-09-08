2341829138b3dbfbc3da5628f7f0c63c357213c8
Sujithashree <Sujithashree3105@gmail.com>
2026-09-08 14:28:46 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                int num = board[i][j] - '1';
+
+                // Find which 3x3 box this cell belongs to
+                int box = (i / 3) * 3 + (j / 3);
+
+                // Check row
+                if (rows[i][num]) {
+                    return false;
+                }
+
+                // Check column
+                if (cols[j][num]) {
+                    return false;
+                }
+
+                // Check 3x3 box
+                if (boxes[box][num]) {
+                    return false;
+                }
+
+                // Mark as seen
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[box][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
